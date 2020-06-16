799dab44411d8f94d3f19a207704739cbccc7ee4
blueshark42 <blueshark42@users.noreply.github.com>
2020-06-17 00:20:35 +0200

    WIP Network

--- src/net.h
+++ src/net.h
@@ -2,11 +2,13 @@
 #define BLXR_SRC_NET_H_
 
 #include <iostream>
+#include <cstring>
 
 #include <Windows.h>
 
 namespace Network {
-int ClientControl();
+void Login(const std::string &name, const std::string &pass);
+//void Connect(const std::string& server, unsigned short port);
 }
 
 #endif //BLXR_SRC_NET_H_
